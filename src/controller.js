cbd0bf929b47518a01b7cb8eb95eb8e0084df6d8
Hachijin-Okamoto <ce255001@meiji.ac.jp>
2025-12-26 19:22:59 +0900

    コメントを追加

--- src/controller.js
+++ src/controller.js
@@ -10,14 +10,17 @@ import { Buffer } from "buffer";
  * @returns 認証を通ったユーザ情報、認証失敗ならnull
  */
 const getAuthenticatedUser = async (authHeader) => {
+  // Authorizationヘッダが存在しない、もしくはBasic認証でない場合
   if (!authHeader || !authHeader.startsWith("Basic ")) return null;
 
+  // Basic認証の情報を取得
   const base64Credentials = authHeader.split(" ")[1];
   const credentials = Buffer.from(base64Credentials, "base64").toString(
     "utf-8",
   );
   const [authUserId, authPassword] = credentials.split(":");
 
+  // ユーザが存在する場合はそのユーザ情報を返す
   const user = await service.getUserById(authUserId);
   if (user && user.password === authPassword) {
     return user;
@@ -45,6 +48,7 @@ export const createNewUser = [
     .withMessage("Incorrect character pattern"),
 
   async (req, res) => {
+    // バリデーションに失敗した場合
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
@@ -57,6 +61,7 @@ export const createNewUser = [
       const { user_id, password } = req.body;
       const result = await service.registerUser(user_id, password);
 
+      // ユーザ登録に失敗した場合（既に同じuser_idを持つアカウントが存在している場合）
       if (result.error) {
         return res.status(400).json({
           message: "Account creation failed",
@@ -79,11 +84,13 @@ export const createNewUser = [
  */
 export const getUserInfoById = async (req, res) => {
   try {
+    // Authorizationヘッダでの認証が失敗した場合
     const authUser = await getAuthenticatedUser(req.headers.authorization);
     if (!authUser) {
       return res.status(401).json({ message: "Authentication failed" });
     }
 
+    // 指定user_idのユーザ情報が存在しない場合
     const targetUser = await service.getUserById(req.params.user_id);
     if (!targetUser) {
       return res.status(404).json({ message: "No user found" });
@@ -111,6 +118,7 @@ const noControlChars = /^[^\x00-\x1F\x7F]*$/;
  * ユーザ情報の更新(PATCH /users/:user_id)
  */
 export const updateUser = [
+  // nicknameとcommentが両方とも指定されていない場合
   body().custom((_value, { req }) => {
     if (
       !req.body ||
@@ -121,19 +129,20 @@ export const updateUser = [
     return true;
   }),
   body("nickname")
-    .optional({ checkFalsy: false })
-    .isLength({ max: 30 })
-    .matches(noControlChars)
+    .optional({ checkFalsy: false }) // 任意
+    .isLength({ max: 30 }) // 30文字以内
+    .matches(noControlChars) // 制御コード以外の任意の文字
     .withMessage(
       "String length limit exceeded or containing invalid characters",
     ),
   body("comment")
-    .optional({ checkFalsy: false })
-    .isLength({ max: 100 })
-    .matches(noControlChars)
+    .optional({ checkFalsy: false }) // 任意
+    .isLength({ max: 100 }) // 100文字以内
+    .matches(noControlChars) // 制御コード以外の任意の文字
     .withMessage(
       "String length limit exceeded or containing invalid characters",
     ),
+  // user_idやpasswordを変更しようとしている場合
   body("user_id").custom((value) => {
     if (value !== undefined)
       throw new Error("Not updatable user_id and password");
@@ -147,17 +156,18 @@ export const updateUser = [
 
   async (req, res) => {
     const { user_id } = req.params;
-
+    // Authorizationヘッダでの認証が失敗した場合
     const authUser = await getAuthenticatedUser(req.headers.authorization);
     if (!authUser) {
       return res.status(401).json({ message: "Authentication failed" });
     }
-
+    // 認証と異なるIDのユーザを指定した場合
     if (authUser.user_id !== user_id) {
       return res.status(403).json({ message: "No permission for update" });
     }
 
     const errors = validationResult(req);
+    // バリデーションに失敗した場合
     if (!errors.isEmpty()) {
       return res.status(400).json({
         message: "User updation failed",
@@ -187,6 +197,7 @@ export const updateUser = [
  */
 export const closeAccount = async (req, res) => {
   try {
+    // Authorizationヘッダでの認証が失敗した場合
     const authUser = await getAuthenticatedUser(req.headers.authorization);
     if (!authUser) {
       return res.status(401).json({
